cc45c4437aec93f7e16e020e6347d9d9a00612f3
Zeelvashi <zeelvashi02@gmail.com>
2022-04-14 15:52:47 +0530

    env

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
-
+const dotenv = require('dotenv')
 // mongoose.connect("mongodb://localhost:27017/adlogin").then(()=>{console.log("Connection is established")});
+
+dotenv.config();
 
 mongoose.connect("mongodb+srv://knzcourier:knz2002@cluster0.apufr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 .then(()=>{
